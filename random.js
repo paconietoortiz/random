a91918016b63ff48d4fc1ca6a761d2339b3a49e0
paconietoortiz <paconieto1993@gmail.com>
2015-05-20 18:21:36 +0200

    random versión 2

--- random.js
+++ random.js
@@ -1,8 +1,8 @@
 var numero = Math.random();
+var str = 'Mayor que 0,5';
 
 if(numero <= 0,5){
-  console.log('\n'+ numero + 'Menor que 0,5 \n');
+str= 'Menor que 0,5 ';
 }
-else{
-  console.log('\n'+ numero + 'Mayor que 0,5 \n');
-}
+
+  console.log('\n'+ numero + str+ '\n');
